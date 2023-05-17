0b1fa1a6a4c7529e6f1ccff159b79e9ee398c354
apastrana-auntap <apastrana@auntap.com>
2023-05-17 01:23:54 -0300

    initial commit

---
+++ src/controllers/handleSiigoLogin.js
@@ -0,0 +1,50 @@
+import puppeteer from 'puppeteer';
+import * as fs from 'fs/promises';
+
+const handleSiigoLogin = async (event) => {
+  const body = event;
+
+  console.warn(
+    `Starting request with username ${body.username} - password ${body.password}`,
+  );
+
+  let browser = await puppeteer.launch({
+    userDataDir: 'tmp/dev',
+    browserWSEndpoint: chrome.endpoint,
+  });
+
+  const page = await browser.newPage();
+  await page.setCacheEnabled(false);
+
+  await page.goto('https://siigonube.siigo.com/ISIIGO2/Login.aspx');
+
+  await page.waitForSelector('#ctl14_txtUserName');
+  await page.type('#ctl14_txtUserName', body.username);
+
+  await page.waitForSelector('#ctl14_txtPassword');
+  await page.type('#ctl14_txtPassword', body.password);
+
+  await Promise.all([
+    page.waitForNavigation({}),
+    page.click('#ctl14_btnLogOn'),
+  ]);
+
+  const cookies = await page.cookies();
+
+  const TKN_cookie = cookies.filter((cookie) => cookie.name.startsWith('TKN'));
+
+  await browser.close();
+
+  await fs.rm('tmp/dev', { recursive: true });
+
+  return getResponse(TKN_cookie.length > 0);
+};
+
+const getResponse = (isLogged) => {
+  return {
+    statusCode: isLogged ? 200 : 403,
+    body: { message: isLogged ? 'OK' : 'Error' },
+  };
+};
+
+export default handleSiigoLogin;
